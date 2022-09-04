80860d8f3e42732259b6a803072b3ae0bd2c5a8f
Sagun Khatri <sagunkhatri@protonmail.com>
2022-09-04 18:15:03 +0545

    [feat]: added release date for upcoming movies

--- MovieHelper.py
+++ MovieHelper.py
@@ -7,13 +7,17 @@ coming_soon = 'coming_soon'
 
 
 class Movie:
-    def __init__(self, movie_name, url, has_ticket, movie_id, showing=False, is3d=None, sold_out=None, reservable=None):
+    def __init__(self, movie_name, url, has_ticket, movie_id, movie_length=None, genre=None, release_date=None, language=None, showing=False, is3d=None, sold_out=None, reservable=None):
         self.movie_name = movie_name
         self.youtube_url = url
         self.has_ticket = has_ticket
         self.movie_id = movie_id
+        self.movie_length = movie_length
+        self.genre = genre
+        self.release_date = release_date
+        self.language = language
         self.book_url = f"https://www.qfxcinemas.com/show?eventId={movie_id}"
-        self.description = f"3D - {is3d}, Reservable - {reservable}, Sold Out - {sold_out}" if showing else "Tickets available" if self.has_ticket else "No tickets available"
+        self.description = f"3D - {is3d}, Reservable - {reservable}, Sold Out - {sold_out}" if showing else f"{movie_length} mins | Tickets Available" if self.has_ticket else f"{genre} | {movie_length} | {release_date} Tickets Available Soon"
 
 
 def get_coming_soon_movies():
@@ -25,6 +29,10 @@ def get_coming_soon_movies():
         movies = [Movie(movie_name=movie['name'], has_ticket=movie['hasBuyTicket'],
                         url=movie['mobileTrailerURL'],
                         movie_id=movie['id'],
+                        language=movie['comments'],
+                        genre=movie['genre'],
+                        release_date=movie['dtLocalRelease'],
+                        movie_length=movie['lengthInMinutes'],
                         )
                   for movie in response['data']]
         return movies
